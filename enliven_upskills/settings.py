0bb50fe4f2fdfc6fbaa5cdf6b0bf2769e4b4afa6
HaseebAhmad522 <haseeb.bhp@gmail.com>
2024-01-15 19:33:37 +0500

    latest code with cors header

--- enliven_upskills/settings.py
+++ enliven_upskills/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     'home', # new
     'rest_framework', # new
     'markdownx', # new
+    'corsheaders', #new
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', # new
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS = True  # new
 
 ROOT_URLCONF = 'enliven_upskills.urls'
 
