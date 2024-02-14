0b7e6c6ba34ae4b55a3cbdde005db21370885de4
root <root@ubuntu-cloudpanel.localhost>
2024-02-14 07:49:17 +0000

    will pull

--- enliven_upskills/settings.py
+++ enliven_upskills/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-icnygq6*zlos_3@_782%9wac$xma59$zalg&$3jvxbuo+o4j$5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
